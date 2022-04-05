e757d64562942ba986c0b0ba3caaa795ffff6357
Marcus Brooks <marcusbrookswebdev@gmail.com>
2022-04-04 21:52:01 -0400

    Added notification for importing items from the shopping list and a link back to the inventory

--- src/components/AddItemInventory.jsx
+++ src/components/AddItemInventory.jsx
@@ -1,37 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./AddItemInventory.css"
 import { Link } from "react-router-dom";
 import NavBar from './NavBar';
+import ItemsAddedNotification from './ItemsAddedNotification.jsx';
 import BackArrow from '../icons/backArrowBlue.png';
 
 const AddItemInventory = ({importFromShoppingList}) => {
+  const [itemsAddedOpen, setItemsAddedOpen] = useState(false);
   return (
     <div className='PageWrapper'>
+      <div className={`addItemInventoryContainer ${itemsAddedOpen ? "blurWall" : ""}`}>
         <div className='HeaderContainer'>
-        <Link to="/inventory/OfficeRefrigerator" className="linkBack2" > <img className="linkBackimage" src={BackArrow} />  Back</Link>
-        <h3 className='Heading'>Add Item(s)</h3>
+          <Link to="/inventory/OfficeRefrigerator" className="linkBack2" > <img className="linkBackimage" src={BackArrow} />  Back</Link>
+          <h3 className='Heading'>Add Item(s)</h3>
         </div>
         <hr className='rule'/>
         <div className='ShoppingListandButton'>
-        <h3 className='ChooseShoppingList'>Choose from Shopping List</h3>
-        <div className='Select'>Select the items from Shopping List</div>
-        <div className='ShoppingListButton' onClick={() => importFromShoppingList()}>
-            Import checked items from Shopping List
-        </div>
-        </div>
+          <h3 className='ChooseShoppingList'>Choose from Shopping List</h3>
+          <div className='Select'>Select the items from Shopping List</div>
+            <div className='ShoppingListButton' onClick={() =>{setItemsAddedOpen(true); importFromShoppingList()}}>
+              Import checked items from Shopping List
+            </div>
+          </div>
         <hr className='rule2'/>
         <div className='ShoppingListandButton'>
-        <h3 className='ChooseShoppingList2'>Or Enter Item Info</h3>
-        <div className='Select'>Manually enter the info for the item to be added</div>
-        <Link to="/inventory/addOfficeRefrigeratorItem" className="linkAdd" color='#007AFF;'> <div className='ShoppingListButton'>
-            Enter Item Info
+          <h3 className='ChooseShoppingList2'>Or Enter Item Info</h3>
+          <div className='Select'>Manually enter the info for the item to be added</div>
+          <Link to="/inventory/addOfficeRefrigeratorItem" className="linkAdd" color='#007AFF;'> 
+            <div className='ShoppingListButton'>
+              Enter Item Info
+            </div>
+          </Link>
         </div>
-        </Link>
-        </div>
-        
-        <NavBar selectedTab={"Inventory"} />
-            
-        
+      </div>
+
+      {itemsAddedOpen ? <ItemsAddedNotification addedToName="Inventory" onClose={() => setItemsAddedOpen(false)} pathToGoTo="/inventory/OfficeRefrigerator" /> : null}
+      <NavBar selectedTab={"Inventory"} />
     </div>
   )
 }
