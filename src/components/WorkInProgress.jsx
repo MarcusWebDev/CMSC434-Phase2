a7b5d4044c69972ea107ed65922f5a19ba441771
Naman Dua <ndua1@umd.edu>
2022-04-07 00:03:00 -0400

    Updated Under Construction Page

--- src/components/WorkInProgress.jsx
+++ src/components/WorkInProgress.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import './WorkInProgress.css'
+import { Link } from "react-router-dom";
+import wrench from '../icons/wrenchIcon.png'
+import NavBar from './NavBar';
 
 const WorkInProgress = () => {
   return (
     <div className='WorkPage'>
-        <h3 className='WorkHeading'>Work In Progress</h3>
+        <Link to="/" className="link" >
+                            <div className="backButtonContainer3">
+                                <img src={require("../icons/backArrowBlue.png")} className="backButtonArrow"/>
+                                <p className="blueText displayInlineBlock">Back</p>
+                            </div>
+                            </Link>
+                            <div><img className='wrenchico' src={wrench} />
+                            <h3 className='wrenchhead'>Under Construction</h3></div>      
+        <NavBar />
         </div>
   )
 }
